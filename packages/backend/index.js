e6479ff44e2462d665c558c64344db0286f61fbf
pawk3k <ravliv.pavlobohdan@gmail.com>
2022-06-13 15:07:55 +0200

    feat: backend release

---
+++ packages/backend/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: "@pawk3k/eslint-config-common1",
+  parser: "@babel/eslint-parser",
+  plugins: ["import"],
+  rules: {},
+  env: {
+    node: true,
+    jest: true,
+  },
+  overrides: [
+    {
+      files: ["migrations/**"],
+      rules: {
+        "no-underscore-dangle": "off",
+      },
+    },
+  ],
+};
